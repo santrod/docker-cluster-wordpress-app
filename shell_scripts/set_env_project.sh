8acc2a3c8563a69d26ce3cf943e878fd27c903b4
santrod <santi.rodriguez.monzon@gmail.com>
2017-10-31 09:24:16 +0100

    Update set_env_project.sh

--- shell_scripts/set_env_project.sh
+++ shell_scripts/set_env_project.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 php /bin/wp --allow-root option update siteurl "$SITEURL" &&
-php /bin/wp --allow-root option update home ""$HOMEURLT";
+php /bin/wp --allow-root option update home "$HOMEURLT";
